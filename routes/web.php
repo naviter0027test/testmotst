c71dc192fa6d2b507c9fe726d4690540010ab7f0
cp5 <cp5@test0068.axcell28.idv.tw>
2022-06-28 15:55:33 +0800

    / redirect /member/login

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/member/login');
 });
 
 Route::group(['prefix' => 'member', 'middleware' => ['check.member']], function() {
